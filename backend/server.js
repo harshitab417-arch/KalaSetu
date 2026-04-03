464d5bdfff8a0f0e959b8d466072f456d78a2c1c
Akshay <akshayk41901@gmail.com>
2026-04-03 19:57:18 +0530

    updated share option and media inmessages

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
-app.use(express.json({ limit: "15mb" }));
+app.use(express.json({ limit: "50mb" }));
 
 app.use("/auth", authRoutes);
 app.use("/posts", postRoutes);
